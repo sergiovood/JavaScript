26a4680de15b570b1243cb24567e82d804cb9cf2
Sergio <sergiovood@gmail.com>
2021-01-06 00:25:39 +0100

    Learn git

---
+++ js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+//Lesson 10 - GIT
+/*
+Uczyc sie gita  - https://githowto.com/ru
+
+komandy - rozpoczecia pracy
+git init - inicjalizacja folderu projektu w ktorym bedziemy pracowac
+
+Ustawienie swojego imia i email - globalnie dla wszystkich projektow (wybralismy ta opcje)
+git config --global user.name "Sergio"
+git config --global user.email sergiovood@gmail.com
+
+Ustawienie danych lokalnie dla projektu, przydatne kiedy musimy podac rozne dane tworcy dla konkretnych projektow
+git config --local user.name "Sergio"
+git config --local user.email sergiovood@gmail.com
+
+
+git status - sprawdzic status plikow
+git add -A - dodac wszystkie pliki do indeksu gita, żeby on zaczął шср śledzić
+git commit - stworzyć punk przywracania wszystkich plików lub konkretnego
+*/
