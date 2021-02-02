5663a117c74a038336a3ea434fd05416331da5de
Sergio <sergiovood@gmail.com>
2021-02-02 17:20:17 +0100

    'Kurs_JS'

--- js/scriptsBackupAllLessons.js
+++ js/scriptsBackupAllLessons.js
@@ -2227,13 +2227,15 @@ const nowEight = new Date();
 console.log(nowEight.setHours(18)); // ustawiamy godziene na 18, a dodatkowo Date() jest pusta a znaczy ze ono pobierze nasza lokalna date, a godzie juz ustawi ktora my podajemy
 console.log(nowEight); // console w visualcode zwrozi 17 godzine po standardowo patrzy na UTC czas, a w przegliadarce konsol zwroci 18 godzine bo patczy na czas lokalny
 
-// 1.3 - PARSE - zadko spotykany
-// metod Daty ktory przeksztawci na date to co my podalismy w nawisach 
+// 1.3 - PARSE
+// metod Daty ktory przeksztawci na date to co my podalismy w nawisach
+// Date.parse() wykorzystywany w sytuacjach kiedy dostajemy date w typie string a chcemy przeksztawic typ num 
 
 // sposob bez metody parse()
 const nowNine = new Date('2021-01-31'); // wynik bedzie z podana data
-// sposob z metodą parse()
-// new Date.parse('2021-01-31'); // taki sam wynik jak na gorze, zadziej spotykany
+
+// sposob z metodą parse() przeksztawca string w num
+new Date.parse('2021-01-31'); // taki sam wynik jak na gorze
 
 
 // Benchmark-i
@@ -2252,6 +2254,92 @@ let end = new Date ();
 alert(`Cykl skonczyl dzialanie za ${end - start} mm`);  // wynik: Cykl skonczyl dzialanie za 20 mm
 
 
+
+
+//--------------------------------------------------------------------------------------------------
+// Lesson 41 - Projekt Food - CZESC 2 
+// Создаем таймер обратного отсчета на сайте
+//
+// Сокращенная запись объектов - https://alligator.io/js/object-property-shorthand-es6/
+
+// TIMER
+    
+    // Alogorytm:
+    // Powinna byc funkcja ktora bedzie ustawiac nasz timer
+    // Funkcja ktora bedzie sprawdzac roznice miedzy data terazniesza u uzytkownika i koncowa data ktora my ustawiamy
+    // Funkcja ktora bedzie uaktualniac czas timera: dni, godziny, minuty, sekundy
+
+    // tworzyy date od ktorej bedziemy odliczac
+    const deadline = '2021-02-08';
+
+    // Tworzymy funkcje ktora bedzie sprawdzac roznice czasu ile zostalo do konca od terazniejszej daty i do konca
+    // przyjmuje w siebie deadline i oblica aktualna godzine 
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()), // najpierw wyliczymy ile milisekund czasu zostalo do konca daty od naszej daty
+              // DNI - bierzemy cala ilosc milisikund 't' i dzielimy na milisekundy w jednym dniu (1000 milisekund * 60 i dostajemy ilosc milisekund w jednej minucie * 60 i dostajemy ilosc minut w jednej godzinie * 24 i dostajemy ilosc godzin w jednym dniu)
+              days = Math.floor(t / (1000 * 60 * 60 * 24)),  // Math.floor - zaokraglenia liczby do najblizszej calej liczby
+              // Godziny - bierzemy cala ilosc milisikund 't' i dzielimy na milisekundy w jednej godzinie ((1000 milisekund * 60 i dostajemy ilosc milisekund w jednej minucie * 60 i dostajemy ilosc minut w jednej godzinie) % 24 dzielimy przez modulo ilosc godzin w dniu, zeby dostac ilosc dni ktora zostanie z dzielenia t / (1000 * 60 * 60))
+              hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+              // Minuty - tutaj odrazu dzielimy wszystkie milisekundy na 1000 milisekund w jednej sekundzie i dzielimy na 60 sekund zeby dostac ilosc sekund, potem korzystamy sie z % 60 minut zeby dostac z calej liczby ilosc sekund ktore zostaly
+              minutes = Math.floor((t / 1000 / 60) % 60),
+              // Sekundy - tutaj cala ilosc milisekund sekund dzielimy na 1000 milisekund w jednej sekundzie, dalej korzystamy z modulo % i 60 sekund w jednej minucie zeby dostac niewystarczajaca ilosc do 60 sekund ktora bedzie wyswietlac sie na stronie
+              seconds = Math.floor((t / 1000) % 60);
+        
+        // zwracamy wszystkie uzyskane dane, zeby dalej z nich korzystac w innych funkcjach'
+        // zwracac bedziemy w formie object
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    // sprawdza kiedy liczba jest ponizej 10 to dodaje na poczatku liczby 0, i zwraca liczbe 01, 02 itd
+    // a jesli liba rowna sie lub wyzej niz 10 to nic nie zmienia i zwraca jej numer
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    // funkcja ktora bedzie pobierac i zmieniac liczby dynamicznie na stronie
+    function setClock(selector, endtime){
+        const timer = document.querySelector(selector), // przyjmuje selector z wylowania funkcji, czyli blok z klasa gdzie jest zawarty caly licznik
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timeInterval = setInterval(updateClock, 1000); // ustawiamy powtorzenia dla glownej funkcji UpdateClock na 1 sekunde
+
+        updateClock(); // odrazu wylowamy funkcje zeby uniknac pierszego  odczekania 1000 ms na stronie pod czas ladowania strony, czyli zatrzymania na poczatkowej dacie, ktora zmieni sie tylko po 1 sekundzie
+
+        // funkcja bedzie juz wpisywac na strone dynamicznie zmieniajacy sie liczby
+        function updateClock() {
+            // do zmiennej t zapisujemy wyliczenie w formie objektu z funkcji getTimeRemaining oraz w srodek wstawimy argument endtime w ktorym bedzie nasz deadline z ktorego beda robione wyliczenia  przez funkcje
+            const t = getTimeRemaining(endtime);
+            
+            // innerHTML usuwa poprzednie znaczenie w kodzie html i wstawia kolejne ktore my zadamy dynamicznie
+            // t.days oraz inni zmienni - so to objekty ktore dostalismy z funkcji getTimeRemaining i teraz dostajemy z objektu dane, czyli odlowanie do objektu.kropka i nazwa klucza w objekcie z ktorej chcemy uzyskac dane
+            days.innerHTML = getZero(t.days); // znaczenia wstawiamy ale najpierw sprawdzamy przez funkcje getZero czy nie trzeba dopisac zera przed liczba od 1 do 9
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            // jesli ilosc milisekund dojdzie do zera lub bedzie juz ujemne bo minie data po za ustawiona, to wtedy wyczyscimy licznik na 0
+            if (t.total <= 0) { // t jest objektem, a total jednym z jego elemntow gdzie zapisuja sie cala ilosc milisekund ktora zostala do konca ustawionej daty w deadline
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    // wywolanie funkcji ktora uruchamia caly licznik
+    // przekazujemy klase bloku z licznikiem i date koncowa jak argumenty do wykorzystania w funkcji
+    setClock('.timer', deadline);
+});
 
 
 //--------------------------------------------------------------------------------------------------
